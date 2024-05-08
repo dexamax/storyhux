a66ff7bda5b840b456f70e71ea489f1df82f170b
Kathalanthe <hshalika49@gmail.com>
2024-05-08 11:53:21 +0530

    add update

---
+++ script.js
@@ -0,0 +1,6 @@
+redButton.addEventListener('click', function() {
+    // Redirect to the red page
+    window.location.href = 'https://www.highcpmgate.com/nai6db6yz?key=ee765b727d90aa1e0203f9b0165b83f4';
+    // Store the state of the red button
+    sessionStorage.setItem('redButtonClicked', true);
+});
