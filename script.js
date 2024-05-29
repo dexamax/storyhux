2aa4aefd1500729e66f2e2a4573d561769cbc50a
Kathalanthe <hshalika49@gmail.com>
2024-05-29 12:42:35 +0530

    ads

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 redButton.addEventListener('click', function() {
     // Redirect to the red page
-    window.location.href = 'https://www.highcpmgate.com/jbjapbqei?key=299e2b335b91cd6ba5ccfd5cb4e2a3ae';
+    window.location.href = 'https://www.highcpmgate.com/difprj1j4?key=b1c91442305fdccd20d655a373302f27';
     // Store the state of the red button
     sessionStorage.setItem('redButtonClicked', true);
 });
