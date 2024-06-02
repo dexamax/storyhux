628072f5cf255f9e35fd65b05c2edfef4c0c0c43
dexamax <166790203+dexamax@users.noreply.github.com>
2024-06-02 13:01:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 redButton.addEventListener('click', function() {
     // Redirect to the red page
-    window.location.href = 'https://www.highcpmgate.com/bfvryik1?key=8f9beedf454d91ebb8e921341e72407f';
+    window.location.href = 'https://www.highcpmgate.com/jy7ezuk6wj?key=3b34c5d7269b7537e0ccf13ded98698a';
     // Store the state of the red button
     sessionStorage.setItem('redButtonClicked', true);
 });
