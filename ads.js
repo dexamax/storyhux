53c1211f87190f8fc49b2cb57999f20950cce3ca
Kathalanthe <hshalika49@gmail.com>
2024-06-08 09:03:44 +0530

    update

---
+++ ads.js
@@ -0,0 +1,13 @@
+// adLoader.js
+
+// Function to load an external script
+function loadAdScript(url) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    document.head.appendChild(script); // Append to head or body as needed
+}
+
+// Load the ad script
+loadAdScript('//pl22417838.highcpmgate.com/83/12/f8/8312f879f3b66c46ffa1d1d8cebc1362.js');
+
