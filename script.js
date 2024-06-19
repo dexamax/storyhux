de9b5a1a2210881cd00a641e7d1bb168c266232e
Kathalanthe <hshalika49@gmail.com>
2024-06-19 16:41:03 +0530

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 redButton.addEventListener('click', function() {
     // Redirect to the red page
-    window.location.href = 'https://www.highrevenuenetwork.com/zckfy7ec9g?key=a1e6f32b4e5abe5d0f65444920493349';
+    window.location.href = 'https://www.highrevenuenetwork.com/bfn9xphudb?key=d1b5baf28a2f5f73531238b877c84366';
     // Store the state of the red button
     sessionStorage.setItem('redButtonClicked', true);
 });
