77ed50de526563c7532c9114ffea0a43cf405ba1
dexamax <166790203+dexamax@users.noreply.github.com>
2024-07-19 14:03:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 redButton.addEventListener('click', function() {
     // Redirect to the red page
-    window.location.href = 'https://www.highrevenuenetwork.com/qqpygk4a?key=cf5cca9e977ea666a0727ba9475c7831';
+    window.location.href = 'https://dexamax.github.io/okpage/';
     // Store the state of the red button
     sessionStorage.setItem('redButtonClicked', true);
 });
