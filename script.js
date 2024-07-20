6454eb3d624b1a851edafd46a3bee4300a2189f9
dexamax <166790203+dexamax@users.noreply.github.com>
2024-07-20 06:40:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 redButton.addEventListener('click', function() {
     // Redirect to the red page
-    window.location.href = 'https://dexamax.github.io/okpage/';
+    window.location.href = 'https://grujeeroowie.com/4/7734397';
     // Store the state of the red button
     sessionStorage.setItem('redButtonClicked', true);
 });
