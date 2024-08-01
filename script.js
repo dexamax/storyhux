3fa9c101767ed2bb4ac01a0770b28f5e424cf881
dexamax <166790203+dexamax@users.noreply.github.com>
2024-08-01 09:06:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 redButton.addEventListener('click', function() {
     // Redirect to the red page
-    window.location.href = 'https://grujeeroowie.com/4/7734397';
+    window.location.href = 'https://www.highratecpm.com/trrc0vqi?key=f338720cb351da92c5123e9da8f7db54';
     // Store the state of the red button
     sessionStorage.setItem('redButtonClicked', true);
 });
